2c147b72da746d8a5b3433eee148a0c53be12f4d
gilfoylex <gilfoylex@outlook.com>
2022-09-06 18:42:34 +0800

    init

---
+++ TestAnimation/Helper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using KeyFrameAnimation;
+
+namespace TestAnimation
+{
+    static class Helper
+    {
+        public static List<KeyFrame> GetKeyFrames()
+        {
+            var duration = 16; // 每一帧间隔
+            var k = new List<KeyFrame>();
+            for (var index = 1; index < 61; ++index)
+            {
+                var bitmap = new BitmapImage(new Uri($"pack://application:,,,/Resources/fire/{index}.png"));
+                k.Add(new KeyFrame
+                {
+                    AFrame = bitmap,
+                    Duration = duration
+                });
+            }
+
+            return k;
+        }
+    }
+}
---
+++ TestAnimation/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestAnimation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool show = true;
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            show = !show;
+            ani.Visibility = show ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private bool start = true;
+        private void ButtonBase_OnClick1(object sender, RoutedEventArgs e)
+        {
+            var x = KeyFrameAnimation.ImageBehavior.GetAnimationController(testpic);
+            if (x != null)
+            {
+                if (start)
+                {
+                    x.Stop();
+                }
+                else
+                {
+                    x.Play();
+                }
+                start = !start;
+            }
+        }
+    }
+}
